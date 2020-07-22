7f364a6369c3922323ecd1ceab8e3564ef82461c
Michele Curtaz <mcurtaz@gmail.com>
2020-07-22 17:02:42 +0200

    bonus con filter

--- main.js
+++ main.js
@@ -57,10 +57,18 @@ function albumsInHtml(albumsArray) { // funzione che stampa in pagina gli album 
 }
 
 
-// BONUS
+// -----------------------------------  BONUS   ------------
 
 function addSelectListener(){
-  $("#genre-select").change(genreFilter); // .change() avvia la funzione quando cambia il valore (value) dell'elemento selezionato in questo caso la select con id="genre-select"
+
+  // prima soluzione commentata
+  //$("#genre-select").change(genreFilter);
+
+
+  $("#genre-select").change(conFilter); // .change() avvia la funzione quando cambia il valore (value) dell'elemento selezionato in questo caso la select con id="genre-select"
+
+
+
 }
 
 function genreFilter(){ // funzione che nasconde o mostra i cd
@@ -70,6 +78,9 @@ function genreFilter(){ // funzione che nasconde o mostra i cd
   var target = $(".cd"); // tutti i div con le info dei cd
 
   var selectedTarget = $(".cd[data-genre=\"" + selected + "\"]")// selezione di tutti i cd che hanno come data-genre il genere selezionato. La stringa è una selezione avanzata del css per attributo con la variabile selected al posto del valore di data-genre
+  // -------
+  // Si poteva anche risolvere con un .each() ciclare su tutti gli elementi e if genere == genere selezionato $(this) mostra se no nascondi
+  // -------
 
   if (selected == "tutti"){ // se si seleziona tutti allora mostro tutti i cd
     target.show();
@@ -81,3 +92,31 @@ function genreFilter(){ // funzione che nasconde o mostra i cd
 
 
 }
+
+
+// BONUS CON METODO .Filter()
+
+// funzione equivalente a genreFilter ma utilizzando .Filter()
+function conFilter() {
+
+  var selected = $(this).val(); // value di $(this). dell'elemento su cui si è scatenato l'evento. quindi è il value della select
+
+  if (selected == "tutti"){ // se si seleziona tutti mostro tutti i gli elementi con classe .cd altrimenti avvio la funzione .filter()
+
+    $(".cd").show();
+
+  } else {
+
+    $(".cd").filter(function(){ // la funzione filter cicla su tutti gli elementi .cd ed esegue le istruzioni della funzione
+
+      var cdGenre = $(this).data("genre"); // la funzione scorre ogni .cd e va a salvarsi di volta in volta il dalore di data-genre. .data() con solo il nome del data tipo -id -genre -info contiene il valore. se invece fai .data("id", "valore") lo assegni
+
+
+      $(this).toggle(cdGenre == selected); // la funzione fa il toggle su tutti i .cd
+      // se la condizione è vera cioè il genere del cd cdGenre è uguale a selected cioè il genere selezionato mostra. altrimenti nasconde
+
+    });
+
+  }
+
+};
