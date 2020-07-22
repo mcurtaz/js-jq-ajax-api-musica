a80e44d9df751ecdd1c81b5433395ff51b8b6488
Michele Curtaz <mcurtaz@gmail.com>
2020-07-22 16:43:39 +0200

    bonus

--- main.js
+++ main.js
@@ -17,6 +17,9 @@ $(document).ready(init);
 
 function init() {
   getAlbums();
+
+  // BONUS
+  addSelectListener();
 }
 
 
@@ -39,6 +42,7 @@ function getAlbums() { // questa funzione fa una richiesta al server e riceve co
   });
 }
 
+
 function albumsInHtml(albumsArray) { // funzione che stampa in pagina gli album compilati con le informazioni che arrivano dal server
 
   var template = $("#template-handlebars").html(); // salvo nella variabile il template in html che gestirò con HANDLEBARS
@@ -50,4 +54,30 @@ function albumsInHtml(albumsArray) { // funzione che stampa in pagina gli album 
     var newAlbum = compiled(albumsArray[i]); // la funzione creata prima salvandola nella variabile compiled sostiuisce le chiavi nelle graffe del template con i valori delle rispettive chiavi nell'oggetto. ad ogni ciclo cambiando l'oggetto cambieranno i valori delle chiavi
     target.append(newAlbum) // stampo nell'html il template compilato
   }
+}
+
+
+// BONUS
+
+function addSelectListener(){
+  $("#genre-select").change(genreFilter); // .change() avvia la funzione quando cambia il valore (value) dell'elemento selezionato in questo caso la select con id="genre-select"
+}
+
+function genreFilter(){ // funzione che nasconde o mostra i cd
+
+  var selected = $(this).val(); // value di $(this). dell'elemento su cui si è scatenato l'evento. quindi è il value della select
+
+  var target = $(".cd"); // tutti i div con le info dei cd
+
+  var selectedTarget = $(".cd[data-genre=\"" + selected + "\"]")// selezione di tutti i cd che hanno come data-genre il genere selezionato. La stringa è una selezione avanzata del css per attributo con la variabile selected al posto del valore di data-genre
+
+  if (selected == "tutti"){ // se si seleziona tutti allora mostro tutti i cd
+    target.show();
+  } else{ // altrimenti nascondo tutti i cd e poi mostro solo quelli con data-genre corrispondente a quello selezionato
+    target.hide();
+    selectedTarget.show();
+  }
+
+
+
 }
