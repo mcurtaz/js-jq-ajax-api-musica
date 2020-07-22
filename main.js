05c7f2c9e0512f1b097feb37a77e01509e78779f
Michele Curtaz <mcurtaz@gmail.com>
2020-07-22 15:48:21 +0200

    Handlebars template

---
+++ main.js
@@ -0,0 +1,20 @@
+// GOAL:
+// Attraverso una chiamata ajax all'Api di boolean avremo a
+// disposizione una decina di dischi musicali.
+// Servendoci di handlebars stampiamo tutto a schermo.
+// In questo momento non è importante la parte grafica.
+
+// Bonus: Creare una select con i seguenti generi: pop, rock,
+// metal e jazz. In base a cosa scegliamo nella select vedremo i
+// corrispondenti cd.
+
+// Chiamata:
+// https://flynn.boolean.careers/exercises/api/array/music
+
+
+
+$(document).ready(init);
+
+function getAlbums() {
+console.log("hello world");
+}
